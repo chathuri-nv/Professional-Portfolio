17e414964c6baebbb0eaefdd8f1735865a2d7f44
chathuri-nv <navamc28@gmail.com>
2026-08-09 15:23:18 +0530

    Finish Home Section

---
+++ script.js
@@ -0,0 +1,95 @@
+let words = document.querySelectorAll(".word");
+
+words.forEach((word) => {
+    let letters = word.textContent.split("");
+    word.textContent = "";
+    letters.forEach((letter) => {
+        let span = document.createElement("span");
+        span.textContent = letter === " " ? "\u00A0" : letter; 
+        span.className = "letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+
+words[currentWordIndex].style.opacity = "1"; 
+
+let changeText = () => {
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
+            letter.className = "letter out";
+        }, i * 80);
+    });
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
+        letter.className = "letter behind";
+        setTimeout(() => {
+            letter.className = "letter in";
+        }, 340 + i * 80);
+    });
+
+    currentWord.style.opacity = "0";
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+setTimeout(() => {
+    setInterval(changeText, 3000);
+}, 2000);
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", function () {
+
+    navLinks.classList.toggle("active");
+
+});
+
+
+const navigationLinks = document.querySelectorAll(".nav-links a");
+
+navigationLinks.forEach(function (link) {
+
+    link.addEventListener("click", function () {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
+
+
+const contactForm = document.getElementById("contact-form");
+const formMessage = document.getElementById("form-message");
+
+contactForm.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+
+    if (name === "" || email === "" || message === "") {
+
+        formMessage.textContent = "Please fill in all fields.";
+
+        return;
+    }
+
+
+    formMessage.textContent =
+        "Thank you! Your message has been submitted successfully.";
+
+    contactForm.reset();
+
+});
+
+const currentYear = document.getElementById("current-year");
+
+currentYear.textContent = new Date().getFullYear();
